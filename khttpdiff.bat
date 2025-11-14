fe6d18d2efbf99770eb96e13bcc465c292d0de61
Konstantine <83359067+KonstantineVashalomidze@users.noreply.github.com>
2025-11-14 23:46:34 +0400

    Delete khttpdiff.bat

--- khttpdiff.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM httpdiff - Windows batch wrapper
-
-REM Get the directory where this script is located
-set SCRIPT_DIR=%~dp0
-
-REM Run the JAR file with all arguments passed through
-java -jar "%SCRIPT_DIR%target\khttpdiff.jar" %*
