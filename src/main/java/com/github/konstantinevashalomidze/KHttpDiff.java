aa23ccfdd56b704affac1920dc158fae56c2e833
KonstantineVasahalomize <vashalomidzekonstantine@gmail.com>
2025-11-15 00:03:27 +0400

    CONTRIBUTING.md

--- src/main/java/com/github/konstantinevashalomidze/KHttpDiff.java
+++ src/main/java/com/github/konstantinevashalomidze/KHttpDiff.java
@@ -4,7 +4,6 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
